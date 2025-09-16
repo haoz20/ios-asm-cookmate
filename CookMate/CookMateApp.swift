98c2f42eabf92aecf7a31b18ed202d9ef006cfc6
haoz20 <zhaonianhao2005@gmail.com>
2025-09-16 20:48:03 +0700

    Initial Commit

---
+++ CookMate/CookMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookMateApp.swift
+//  CookMate
+//
+//  Created by Swan Htet Aung on 16/9/25.
+//
+
+import SwiftUI
+
+@main
+struct CookMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
